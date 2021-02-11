036b21dc98537f29d261f9893dfae977f2a5031f
Sun Gyu Park <31524410+samfan0429@users.noreply.github.com>
2021-02-12 00:36:01 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 // # include <iostream>
+/*
+    @Author SoupAndWife (Real Name: Sun Gyu Park)
+    Illustrator: Pokemon Drunk Oak
+    
+    All major copyrights belong to Nintendo. This is not used for marketing or for any business. All secondary products belonging in the resource folder belongs to Pokemon Drunk Oak.
+    If one wants to use the resource files (images, audio files), please consult with Youtuber, Pokemon Drunk Oak, the original illustrator of the resources.
+    
+    모든 기본 저작권은 닌텐도에 있고 2차 창작용으로 만들어진 일러스트들은 Pokemon Drunk Oak님이 작성해주신고로 일러스트를 사용하시고 싶은 분은 직접적인 상담 부탁드립니다.
+    이 파일은 상업적인 목적과 일절 상관이 없는 개인적인 취미 생활이니 이를 빼돌려서 악용할 경우 법정에서 면담을 나누게 돼는 힘든 삶이 양쪽에게 기다립니다.
+    
+    E-mail (Code Author): jappan4212@gmail.com
+    E-mail (Pokemon Drunk Oak): drunkoak@gmail.com
+*/
 # include <Windows.h>
 
 # include <SFML/Graphics.hpp>
